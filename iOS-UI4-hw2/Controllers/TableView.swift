c228fa6d974069479c9ddc970e8883c3938eea06
d1onnisss <lim_al@auca.kg>
2024-05-22 22:58:21 +0600

    first commit

---
+++ iOS-UI4-hw2/Controllers/TableView.swift
@@ -0,0 +1,8 @@
+//
+//  TableView.swift
+//  iOS-UI4-hw2
+//
+//  Created by Alexey Lim on 15/5/24.
+//
+
+import Foundation
---
+++ iOS-UI4-hw2/Model/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contacts.swift
+//  iOS-UI4-hw2
+//
+//  Created by Alexey Lim on 15/5/24.
+//
+
+import Foundation
+
+struct Contacts {
+    var image: String
+    var fullName: String
+    var phoneNumber: String
+}
